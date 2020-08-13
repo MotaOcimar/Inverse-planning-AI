f545e8429d4edff7fb14d43d09a45bdbd71be06a
Felipe Bomfim <felipebomfim4161@gmail.com>
2020-08-13 18:22:26 -0300

    Initial versions of neural networks, still needing to be tested

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import os
+from time import time
+
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import TensorBoard
+from lenet5 import make_lenet5
+from utils import read_mnist, save_model_to_json
+
 from monte_carlo import generate_input_data
 from neural_network import inverse_planning_model
 
@@ -10,6 +21,7 @@ model = inverse_planning_model()
 
 def train():
     #treina a NN usando os dados  gerados por Monte Carlo
+
     pass
 
 
--- neural_network.py
+++ neural_network.py
@@ -1,14 +1,88 @@
-from keras import models, layers
+from tensorflow.keras import layers, losses, optimizers, activations, metrics, regularizers
+from tensorflow.keras.models import Sequential
 
 
-def inverse_planning_model():
+def inverse_planning_model(width, height, output_len):
     """
     Create a Keras model for inverse planning
 
     :return: Keras model
     :rtype: Keras model
     """
-    model = models.Sequential()
-    # TODO
+    model = Sequential()
+    
+    # Todo: implement LeNet-5 model
+    activation_func = activations.tanh
+    lambda_l2 = 0
+
+    # 1
+    model.add(layers.Conv2D( kernel_size=(5, 5), kernel_regularizer=regularizers.l2(lambda_l2),
+                            strides=(1, 1), activation=activation_func,  input_shape=(width, height, 1)))
+    # 2
+    model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # 3
+    model.add(layers.Flatten())
+
+    model.add(layers.Dense(units=84, activation=activation_func))
+    model.add(layers.Dense(units=84, activation=activation_func))
+
+    # 4
+    model.add(layers.Dense(units=output_len, activation=activations.softmax))
+
+    # Para criar covolucao 2D
+    # model.add(layers.Conv2D(filters=nf, kernel_size=(fx, fy), strides=(sx, sy), activation=activations.fun))
+
+    # Para criar average pooling
+    # model.add(layers.AveragePooling2D(pool_size=(px, py), strides=(sx,sy)))
+    # Para definir camada de transição entre as camadas convolucionais e as densas.
+    # model.add(layers.Flatten())
+    # model.add(layers.Dense(units=num_neurons,activation=activations.fun))
+    return model
+
+def inverse_planning_model_Lenet5(width, height, output_len):
+    """
+    Create a Keras model for inverse planning
+
+    :return: Keras model
+    :rtype: Keras model
+    """
+    model = Sequential()
+    
+    # Todo: implement LeNet-5 model
+    activation_func = activations.tanh
+    lambda_l2 = 0
+    
+    # 1
+    model.add(layers.Conv2D(filters=6, kernel_size=(5, 5), kernel_regularizer=regularizers.l2(lambda_l2),
+                            strides=(1, 1), activation=activation_func,  input_shape=(width, height, 1)))
+    # 2
+    model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # 3
+    model.add(layers.Conv2D(filters=16, kernel_size=(5, 5), kernel_regularizer=regularizers.l2(lambda_l2),
+                            strides=(1, 1), activation=activation_func))
+
+    # 4
+    model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # 5
+    model.add(layers.Conv2D(filters=120, kernel_size=(5, 5), kernel_regularizer=regularizers.l2(lambda_l2),
+                            strides=(1, 1), activation=activation_func))
+
+    # 6
+    model.add(layers.Flatten())
+    model.add(layers.Dense(units=84, activation=activation_func))
+
+    # 7
+    model.add(layers.Dense(units=10, activation=activations.softmax))
+    # Para criar covolucao 2D
+    # model.add(layers.Conv2D(filters=nf, kernel_size=(fx, fy), strides=(sx, sy), activation=activations.fun))
+
+    # Para criar average pooling
+    # model.add(layers.AveragePooling2D(pool_size=(px, py), strides=(sx,sy)))
+    # Para definir camada de transição entre as camadas convolucionais e as densas.
+# model.add(layers.Flatten())
+# model.add(layers.Dense(units=num_neurons,activation=activations.fun))
     return model
 

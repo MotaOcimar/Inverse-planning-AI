24e6b3d4f4f2a9910bc4dbda48aeaa5697ab5a51
omsf <ocimar.acad@gmail.com>
2020-08-16 07:38:40 -0300

    Remove lenet network

--- neural_network.py
+++ neural_network.py
@@ -29,19 +29,3 @@ def inverse_planning_model(height, width, output_len):
                            kernel_regularizer=regularizers.l2(lambda_l2)))
 
     return model
-
-
-def make_lenet5(input_shape, output_len):
-    model = Sequential()
-
-    model.add(layers.Conv2D(filters=6, kernel_size=(3, 3), strides=(1, 1), activation='relu',
-                            input_shape=input_shape))
-    model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(layers.Conv2D(filters=16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
-    model.add(layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(layers.Conv2D(filters=120, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(units=14, activation=activations.tanh))
-    model.add(layers.Dense(units=output_len, activation=activations.softmax))
-
-    return model
